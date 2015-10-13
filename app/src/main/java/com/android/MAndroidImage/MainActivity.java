8ce861c8dc7fc39f9b6da02f8a27670307c8fbf0
Sarthak Grover <sgrover@blackpearlsystems.com>
2015-10-12 19:29:57 -0700

    Added camera functionality to take pic and save it locally

--- app/src/main/java/com/android/MAndroidImage/MainActivity.java
+++ app/src/main/java/com/android/MAndroidImage/MainActivity.java
@@ -2,19 +2,29 @@ package com.android.MAndroidImage;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.R.drawable.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends Activity {
     private static final String TAG = "Log-Messages";
@@ -23,6 +33,9 @@ public class MainActivity extends Activity {
     private ImageView image;
     private ProgressDialog mProgressDialog;
     private long startTime = 0l;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    String mCurrentPhotoPath;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +50,9 @@ public class MainActivity extends Activity {
         // Locate the Button in activity_main.xml
         Button button = (Button) findViewById(R.id.button);
 
+        ImageButton cameraButton = (ImageButton) findViewById(R.id.capture_front);
+
+
         // Capture button click
         button.setOnClickListener(new OnClickListener() {
             public void onClick(View arg0) {
@@ -45,7 +61,71 @@ public class MainActivity extends Activity {
                 new DownloadImage().execute(imageName);
             }
         });
+
+        cameraButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View arg0) {
+
+                dispatchTakePictureIntent();
+            }
+        });
     }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.i(TAG, "Exception thrown in dispatchTakePictureIntent()" + ex.toString());
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
+    // Todo: Show thumbnail
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        System.out.println("result code: " + resultCode + " and request code: " + requestCode);
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            image.setImageBitmap(imageBitmap);
+//        }
+//        else
+//            System.out.println("NO");
+//    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+
+
 
     // DownloadImage AsyncTask
     private class DownloadImage extends AsyncTask<String, Void, Bitmap> {
@@ -81,6 +161,9 @@ public class MainActivity extends Activity {
             return bitmap;
         }
 
+
+
+
         @Override
         protected void onPostExecute(Bitmap result) {
             // Set the bitmap into ImageView
@@ -95,4 +178,6 @@ public class MainActivity extends Activity {
             downloadTime.setText(String.valueOf(duration) + " ms");
         }
     }
+
+
 }
